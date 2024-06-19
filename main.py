74acf43fc58b209ec45435c88c1dc62525c46880
stevietchinda <stevietchinda215@gmail.com>
2024-06-19 15:31:20 +0200

    classe main

---
+++ main.py
@@ -0,0 +1,10 @@
+from views import author_view
+
+print(""""""""""""""""""""""Gestion des auteurs""""""""""""""""")
+def main():
+    author_views=author_view.menu()
+
+if __name__ == "__main__":
+   main()
+    
+
